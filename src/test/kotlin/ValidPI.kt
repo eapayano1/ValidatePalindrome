04669fe458b80b81a740f23216da40112da7f64c
Emiko47 <emilpayano70@gmail.com>
2020-12-15 23:15:51 -0400

    PrimerTesteo

---
+++ src/test/kotlin/ValidPI.kt
@@ -0,0 +1,14 @@
+
+fun validatePalindromeLetter(a:String):Boolean{
+    val b:CharArray=a.toLowerCase().toCharArray()
+    var flag=true
+    for(i in 0..b.size-1){
+        if(b[i]!=b[b.size-1-i]){
+            flag=false
+        }
+    }
+    if(flag){
+        return true
+    }
+    return false
+}
