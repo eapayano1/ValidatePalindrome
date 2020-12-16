1e0fd7ef5c5cb1f766895c4bf45592900d592781
Emiko47 <emilpayano70@gmail.com>
2020-12-15 23:25:39 -0400

    SegundoTesteo

--- src/test/kotlin/ValidPI.kt
+++ src/test/kotlin/ValidPI.kt
@@ -1,6 +1,6 @@
 
-fun validatePalindromeLetter(a:String):Boolean{
-    val b:CharArray=a.toLowerCase().toCharArray()
+fun validatePalindromeLetter(palindromLetter: String):Boolean{
+    val b:CharArray=palindromLetter.toLowerCase().toCharArray()
     var flag=true
     for(i in 0..b.size-1){
         if(b[i]!=b[b.size-1-i]){
@@ -8,6 +8,22 @@ fun validatePalindromeLetter(a:String):Boolean{
         }
     }
     if(flag){
+        return true
+    }
+    return false
+}
+
+fun validatePalindromeNumber(palindromNumeber:Int):Boolean{
+    var digit=0
+    var reve=0
+    var num=palindromNumeber
+
+    while(num!=0){
+        digit=num%10
+        reve=(reve*10) + digit
+        num=num/10
+    }
+    if(reve==palindromNumeber){
         return true
     }
     return false
