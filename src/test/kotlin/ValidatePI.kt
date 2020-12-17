3ae03925375ac42a6a36c67c762744929b4fa4fd
Emiko47 <emilpayano70@gmail.com>
2020-12-17 19:57:49 -0400

    SegundoTesteo1

--- src/test/kotlin/ValidatePI.kt
+++ src/test/kotlin/ValidatePI.kt
@@ -1,13 +1,28 @@
 
-fun validatePalindromeLetter(a:String): Boolean{
-    val b : CharArray = a.toLowerCase().toCharArray()
+fun validatePalindromeLetter(letter:String): Boolean{
+    val arraychar : CharArray = letter.toLowerCase().toCharArray()
     var flag = true
-    for(i in 0..b.size - 1){
-        if(b[i] != b[b.size-1-i]){
+    for(i in 0..arraychar.size - 1){
+        if(arraychar[i] != arraychar[arraychar.size-1-i]){
             flag = false
         }
     }
     if(flag){
+        return true
+    }
+    return false
+}
+
+fun validatePalindromeNumber(number:Int):Boolean{
+    var digit=0
+    var reve=0
+    var num=number
+    while(num!=0){
+        digit=num%10
+        reve=(reve*10)+digit
+        num=num/10
+    }
+    if(reve==number){
         return true
     }
     return false
