b6987f3ec67c3230d8a75de0d77d240a9efb4079
Emiko47 <emilpayano70@gmail.com>
2020-12-17 19:44:59 -0400

    PrimerTesteo1

---
+++ src/test/kotlin/ValidatePTest.kt
@@ -0,0 +1,11 @@
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class SampleTest{
+    @Test
+    fun testValidatePalindromeLetter(){
+        val actual=validatePalindromeLetter("reconocer")
+        val expected=true
+        assertEquals(expected,actual)
+    }
+}
